f306bd6396b26381d6d8eb63e61da01378ac1cde
Cipher <nyakangomicah4@gmail.com>
2025-03-05 18:33:40 +0300

    second commit

--- stk_initiate.php
+++ stk_initiate.php
@@ -5,8 +5,8 @@ if(isset($_POST['submit'])){
   date_default_timezone_set('Africa/Nairobi');
 
   # access token
-  $consumerKey = 'nk16Y74eSbTaGQgc9WF8j6FigApqOMWr'; //Fill with your app Consumer Key
-  $consumerSecret = '40fD1vRXCq90XFaU'; // Fill with your app Secret
+  $consumerKey = 'eDTxlPP36FQQBJPW3ntbBb571HZT2EG7EshlJ3gY2mEUzo7G'; //Fill with your app Consumer Key
+  $consumerSecret = 'gGBZbc1RVq3AA5a4zYd69IrbYfGWMNO6N0pZGKVSJZbXtlhdvRw9cNJ3tCnGhld3'; // Fill with your app Secret
 
   # define the variales
   # provide the following details, this part is found on your test credentials on the developer account
@@ -42,7 +42,7 @@ if(isset($_POST['submit'])){
   $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 
   # callback url
-  $CallBackURL = 'https://morning-basin-87523.herokuapp.com/callback_url.php';  
+  $CallBackURL = 'https://secret-badlands-80365-33b636054694.herokuapp.com/callback_url.php';  
 
   $curl = curl_init($access_token_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
